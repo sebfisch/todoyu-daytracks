85764f4d15ae79cdb2ee55670fda665e82512d00
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2009-11-09 10:10:00 +0000

    * Fixes

--- assets/js/History.js
+++ assets/js/History.js
@@ -19,11 +19,17 @@
 ***************************************************************/
 
 Todoyu.Ext.daytracks.History = {
-	
+
 	popup: null,
 	
+	popupID: 'daytracksHistory',
+
 	showDetails: false,
 	
+	getPopup: function() {
+		return Todoyu.Popup.getPopup(this.popupID);
+	},
+
 	show: function() {
 		var url		= Todoyu.getUrl('daytracks', 'history');
 		var options	= {
@@ -34,39 +40,39 @@ Todoyu.Ext.daytracks.History = {
 		var idPopup	= 'popup-daytracks-history';
 		var title	= '[LLL:daytracks.history.title]';
 
-		this.popup	= Todoyu.Popup.openWindow(idPopup, title, 420, 490, url, options);
+		this.popup	= Todoyu.Popup.openWindow(this.popupID, title, 420, 490, url, options);
 	},
-	
-	
+
+
 	/**
 	 *	Update history
 	 */
 	update: function() {
 		var date	= this.getSelectedDate();
-		
+
 		var url		= Todoyu.getUrl('daytracks', 'history');
 		var options = {
 			'parameters': {
 				'cmd':		'history',
 				'year':		date.year,
-				'month':	date.month,	
+				'month':	date.month,
 				'details':	this.showDetails ? 1 : 0
 			}
 		};
 
-		Todoyu.Popup.updateContent(url, options);
+		Todoyu.Popup.updateContent(this.popupID, url, options);
 	},
-	
-	
+
+
 	getSelectedDate: function() {
 		var range	= $F('daytracks-history-selector').split('-');
-		
+
 		return {
 			'year': range[0],
-			'month': range[1]			
+			'month': range[1]
 		};
 	},
-	
+
 
 	/**
 	 *	Toggle details
@@ -76,11 +82,11 @@ Todoyu.Ext.daytracks.History = {
 
 		this.update();
 	},
-	
+
 	goToTask: function(idTask) {
 		this.popup.close();
-		
+
 		Todoyu.goTo('project', 'ext', {task:idTask,tab:'timetracking'}, 'task-' + idTask + '-timetracking');
 	}
-	
+
 };
