44a0accb7be33888883c148cef05fd2a19744952
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2012-07-11 15:38:05 +0000

    * Version update

--- config/extinfo.php
+++ config/extinfo.php
@@ -31,12 +31,12 @@ Todoyu::$CONFIG['EXT']['daytracks']['info'] = array(
 		'company'	=> 'snowflake productions GmbH, Zurich'
 	),
 	'state'			=> 'stable',
-	'version'		=> '1.2.0',
+	'version'		=> '1.3.0',
 	'constraints'	=> array(
 		'core'		=> '2.2.0',
 		'depends'	=> array(
-			'project'		=> '1.3.0',
-			'timetracking'	=> '1.2.0'
+			'project'		=> '1.4.0',
+			'timetracking'	=> '1.3.0'
 		)
 	),
 	'urlDocumentation'	=> 'http://doc.todoyu.com/?daytracks'
